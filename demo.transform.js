7543282d83258b51f5ffaf7433eb937c690ffe3f
Thomas Aylott <thomas@thingsthatdostuff.com>
2019-02-13 16:46:20 -0500

    HEllo World

---
+++ demo.transform.js
@@ -0,0 +1,8 @@
+module.exports = function(fileInfo, api, options) {
+  console.log({fileInfo, api, options});
+  
+  // transform `fileInfo.source` here
+  // ...
+  // return changed source
+  return fileInfo.source;
+};
